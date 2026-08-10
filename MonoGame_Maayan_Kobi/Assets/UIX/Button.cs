31c19a8015a64a34adc5f13dae2849efd50160e7
kobicool6000 <52893298+kobicool6000@users.noreply.github.com>
2026-08-10 18:50:23 +0300

    Adding the menu+Button

---
+++ MonoGame_Maayan_Kobi/Assets/UIX/Button.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame_Maayan_Kobi
+{
+    public class Button : Sprite
+    {
+        public delegate void Clicked();
+        //
+        public Text content = new Text();
+        bool isPressed;
+
+        public Button(string spriteName) : base(spriteName)
+        {
+        }
+
+        public event Clicked ClickAction;
+
+        public override void Update(GameTime gameTime)
+        {
+            var mouseState = Mouse.GetState();
+            var mousePoint = new Point(mouseState.X, mouseState.Y);
+            Rectangle textureBorder =
+                new Rectangle((int)tm.position.X, (int)tm.position.Y, this.texture.Width, this.texture.Height); //This gives the area of the texture for hovering and clicking logic
+            isPressed = mouseState.LeftButton == ButtonState.Pressed; //Checks for presses
+
+            if (textureBorder.Contains(mousePoint)) //If hovering
+            {
+                base.color = Color.Black;
+                if (isPressed)
+                    ClickAction.Invoke();
+            }
+            else base.color = Color.White;
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            content.tm.position = tm.position; // Font always in the center of a button
+            base.Draw(spriteBatch);
+            content.Draw(spriteBatch);
+        }
+    }
+}
---
+++ MonoGame_Maayan_Kobi/Assets/UIX/Menu.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
+
+namespace MonoGame_Maayan_Kobi
+{
+    public class Menu: IDrawable
+    {
+        public delegate void Started();
+        public Texture2D sharedTexture; 
+        public SpriteFont sharedFont; // All buttons have shared resources
+        public Game gameOneLogic;
+        public bool interactable = true;
+        public event Started started;
+        //
+        public Button startButton = new Button();
+        public Button quitButton = new Button();
+        //
+        public void MenuStart()
+        {
+            started.Invoke();
+        }
+        public void MenuSettings()
+        {
+            Debug.WriteLine("Oh noooo where are the settings?? D:");
+        }
+        public void MenuQuit()
+        {
+            gameOneLogic.Exit();
+        }
+        public void Start(Game wantedGame)
+        {
+            gameOneLogic = wantedGame; // This allows menu to quit the game
+            //Subscribing to events
+            startButton.ClickAction += MenuStart;
+            quitButton.ClickAction += MenuQuit;
+            //Textures
+            startButton.texture = sharedTexture;
+            quitButton.texture = sharedTexture;
+            //Fonts
+            startButton.content.font = sharedFont;
+            quitButton.content.font = sharedFont;
+            //Text
+            startButton.content.text = "Start";
+            quitButton.content.text = "Quit";
+        }
+        public void Update(GameTime gameTime)
+        {
+            if (!interactable) return;
+            startButton.Update(gameTime);
+            quitButton.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (!interactable) return;
+            startButton.Draw(spriteBatch);
+            quitButton.Draw(spriteBatch);
+        }
+    }
+}
