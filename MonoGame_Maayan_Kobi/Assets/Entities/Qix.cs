f24514db882d9dfa20341e8d88c24992361ee2e2
kobicool6000 <52893298+kobicool6000@users.noreply.github.com>
2026-08-10 16:41:55 +0300

    Gonna do something

--- MonoGame_Maayan_Kobi/Assets/Entities/Qix.cs
+++ MonoGame_Maayan_Kobi/Assets/Entities/Qix.cs
@@ -12,25 +12,34 @@ namespace MonoGame_Maayan_Kobi
     public class Qix: Enemy
     {
         float speedMovement = 300;
-        Vector2 direction = new Vector2();
-        private Vector2 prevPosition = Game1._screenCenter;
-        
-        
-        public override void Update(GameTime gameTime)
-        {
-            if (Utils.IsOutOfBounds(tm.position, prevPosition, this))
-            {
-                tm.position =  prevPosition;
-            }
-            prevPosition =  tm.position;
-        }
-        
-        
+        Vector2 velocity = new Vector2();
+        private Vector2 prevPos = Game1._screenCenter;
         
         protected override void Action(GameTime gameTime)
         {
+            if (Utils.IsOutOfBounds(tm.position, prevPos, this))
+                tm.position = prevPos;
+            prevPos = tm.position;
+
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            tm.position += direction * speedMovement * deltaTime; 
+            if (true)
+            {
+
+            }
+            if (true)
+            {
+
+            }
+            if (true)
+            {
+
+            }
+            if (true)
+            {
+
+            }
+
+            tm.position += velocity * speedMovement * deltaTime; 
             //insert collision logic here
         }
     }
--- MonoGame_Maayan_Kobi/Assets/Logic/Utils.cs
+++ MonoGame_Maayan_Kobi/Assets/Logic/Utils.cs
@@ -4,14 +4,14 @@ namespace MonoGame_Maayan_Kobi;
 
 public static class Utils
 {
-    public static Vector2 CheckCurrentSquare(Vector2 pos)
+    public static Vector2 CheckCurrentSquare(Vector2 pos) //Checks with Vector2 where the player is in the grid
     {
         float X = pos.X / Board.singleSquareWidth;
         float Y = pos.Y / Board.singleSquareHeight;
         return new Vector2(X, Y);
     }
 
-    public static bool IsOutOfBounds(Vector2 currentPos, Vector2 prevPos, Sprite thisSprite)
+    public static bool IsOutOfBounds(Vector2 currentPos, Vector2 prevPos, Sprite thisSprite) //Checks if the current position is outside the play bounds
     {
         if (currentPos.X < 0 || currentPos.Y < 0 || currentPos.X > Game1.ScreenWidth - thisSprite.texture.Width/2f + 15|| currentPos.Y > Game1.ScreenHeight - thisSprite.texture.Height/2f - 10)
         {
