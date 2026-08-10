798b625b7591065d9ea512aa1fa4e01d81c81df8
kobicool6000 <52893298+kobicool6000@users.noreply.github.com>
2026-08-10 17:47:52 +0300

    Work on Qix

--- MonoGame_Maayan_Kobi/Assets/Entities/Qix.cs
+++ MonoGame_Maayan_Kobi/Assets/Entities/Qix.cs
@@ -12,35 +12,40 @@ namespace MonoGame_Maayan_Kobi
     public class Qix: Enemy
     {
         float speedMovement = 300;
-        Vector2 velocity = new Vector2();
+        int vertiDelta = 1, HorizDelta = 1;
+        Vector2 velocity = new Vector2(1,1);
         private Vector2 prevPos = Game1._screenCenter;
         
         protected override void Action(GameTime gameTime)
         {
-            if (Utils.IsOutOfBounds(tm.position, prevPos, this))
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Board.Status currentSquareStatus = Utils.WhatSquareAmI(tm.position, out int curX, out int curY);
+            if (Utils.IsOutOfBounds(tm.position, prevPos, this) || currentSquareStatus != Board.Status.Captured)
                 tm.position = prevPos;
             prevPos = tm.position;
-
-            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (true)
+            /*
+            if (true) // vertical
             {
-
+                vertiDelta = 1;
+                velocity.X = vertiDelta;
             }
             if (true)
             {
-
+                vertiDelta = -1;
+                velocity.X = vertiDelta;
+            }
+            if (true)// horizontal
+            {
+                HorizDelta = 1;
+                velocity.Y = HorizDelta;
             }
             if (true)
             {
-
+                HorizDelta = -1;
+                velocity.Y = HorizDelta;
             }
-            if (true)
-            {
-
-            }
-
-            tm.position += velocity * speedMovement * deltaTime; 
-            //insert collision logic here
+            */
+            tm.position += velocity; 
         }
     }
     
