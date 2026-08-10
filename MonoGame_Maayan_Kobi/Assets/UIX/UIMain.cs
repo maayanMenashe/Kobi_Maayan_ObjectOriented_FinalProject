539a5548edc9df3d48abc8dc41ae04643aedfc5b
kobicool6000 <52893298+kobicool6000@users.noreply.github.com>
2026-08-11 00:53:09 +0300

    UI improvements

--- MonoGame_Maayan_Kobi/Assets/UIX/UIMain.cs
+++ MonoGame_Maayan_Kobi/Assets/UIX/UIMain.cs
@@ -8,24 +8,29 @@ using System.Threading.Tasks;
 
 namespace MonoGame_Maayan_Kobi.Assets.UIX
 {
-    public class UIMain: IUpdatable, IDrawable
+    public class UIMain: IDrawable
     {
         public SpriteFont wantedFont;
         Text lives = new Text();
+        Text claimed = new Text();
 
         public void Start()
         {
             lives.font = wantedFont;
-            lives.text = "lmao";
+            claimed.font = wantedFont;
             lives.tm.position = new Vector2(100, 50);
+            claimed.tm.position = new Vector2(200, 135);
         }
 
-        public void Update(GameTime gameTime)
+        public void Update(int incLives)
         {
+            lives.text = "Lives: " + incLives;
+            claimed.text = "Claimed: " + Game1._screenCenter.X;
         }
         public void Draw(SpriteBatch spriteBatch)
         {
             lives.Draw(spriteBatch);
+            claimed.Draw(spriteBatch);
         }
     }
 }
